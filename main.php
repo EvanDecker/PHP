e57f6447e0b60be1bc3d5d1f127668ebf45d2fd3
EvanDecker <evan.decker@yahoo.com>
2024-10-15 10:50:34 -0500

    print the multiplication result string

--- main.php
+++ main.php
@@ -5,3 +5,5 @@ require('./constants.php');
 $array = [ 'coolconst' => coolconst, 'prettygood' => prettygood, 'coolestconst' => coolestconst, 'bestnumber' => bestnumber ];
 
 $result = (int) $array['bestnumber'] * $array['prettygood'];
+
+echo "The result of $array[bestnumber] * $array[prettygood] is $result.";
