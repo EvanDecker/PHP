bdbaaca5f481ab91e938527d7b8d5f92cff28740
EvanDecker <evan.decker@yahoo.com>
2024-10-15 11:43:23 -0500

    complete logic using a `for` loop

--- loop-control.php
+++ loop-control.php
@@ -4,20 +4,20 @@ for($x = 0; $x < 100; $x+=3) {
   $numbers[] = $x;
 }
 
-foreach($numbers as $number) {
-  if($number === 0) echo "First number\n";
-  else if($number === 3) echo "Three\n";
-  else if($number === 9) {
+for($x = 0; $x < count($numbers); $x++) {
+  if($numbers[$x] === 0) echo "First number\n";
+  else if($numbers[$x] === 3) echo "Three\n";
+  else if($numbers[$x] === 9) {
     for($x = 0; $x < 3; $x++) {
       echo "Nine\n";
     }
   }
-  else if($number === 15) {
+  else if($numbers[$x] === 15) {
     for($x = 0; $x < 5; $x++) {
       echo "Fifteen\n";
     }
   }
-  else if($number === 99) echo "Last number\n";
-  else if($number % 7 === 0) echo "Sevens are lucky, this number has " . $number / 7 . "\n";
-  else if($number % 10 === 0) echo "$number is a round number\n";
+  else if($numbers[$x] === 99) echo "Last number\n";
+  else if($numbers[$x] % 7 === 0) echo "Sevens are lucky, this number has " . $numbers[$x] / 7 . "\n";
+  else if($numbers[$x] % 10 === 0) echo "$numbers[$x] is a round number\n";
 }
