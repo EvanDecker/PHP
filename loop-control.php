1136b531148e19e0272191a86a9e19156a29780e
EvanDecker <evan.decker@yahoo.com>
2024-10-15 11:21:56 -0500

    foreach to echo the numbers

--- loop-control.php
+++ loop-control.php
@@ -4,3 +4,20 @@ for($x = 0; $x < 100; $x+=3) {
   $numbers[] = $x;
 }
 
+foreach($numbers as $number) {
+  switch($number) {
+    case 3:
+      echo "Three";
+      break;
+    case 9:
+      for($x = 0; $x < 3; $x++) {
+        echo "Nine";
+      }
+      break;
+    case 15:
+      for($x = 0; $x < 5; $x++) {
+        echo "Fifteen";
+      }
+      break;
+  }
+}
