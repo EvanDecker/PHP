1db0028e936db27793eaf7da274efe671132d290
EvanDecker <evan.decker@yahoo.com>
2024-10-15 11:36:12 -0500

    complete logic using `foreach`

--- loop-control.php
+++ loop-control.php
@@ -5,19 +5,19 @@ for($x = 0; $x < 100; $x+=3) {
 }
 
 foreach($numbers as $number) {
-  switch($number) {
-    case 3:
-      echo "Three";
-      break;
-    case 9:
-      for($x = 0; $x < 3; $x++) {
-        echo "Nine";
-      }
-      break;
-    case 15:
-      for($x = 0; $x < 5; $x++) {
-        echo "Fifteen";
-      }
-      break;
+  if($number === 0) echo "First number\n";
+  else if($number === 3) echo "Three\n";
+  else if($number === 9) {
+    for($x = 0; $x < 3; $x++) {
+      echo "Nine\n";
+    }
   }
+  else if($number === 15) {
+    for($x = 0; $x < 5; $x++) {
+      echo "Fifteen\n";
+    }
+  }
+  else if($number === 99) echo "Last number\n";
+  else if($number % 7 === 0) echo "Sevens are lucky, this number has " . $number / 7 . "\n";
+  else if($number % 10 === 0) echo "$number is a round number\n";
 }
