bd9ab79ee450b6a1976a261a6a25de13e299e651
EvanDecker <evan.decker@yahoo.com>
2024-10-15 13:46:52 -0500

    `Math` class refactored to use `doOperation` method

--- math.php
+++ math.php
@@ -1,34 +1,54 @@
 <?php
 class Math {
-  public function add($arg1, $arg2, ...$args) {
-    $total = $arg1 + $arg2;
-    foreach($args as $arg) {
-      $total += $arg;
+  const add = '+';
+  const subtract = '-';
+  const multiply = '*';
+  const divide = '/';
+
+  private function doOperation($operationType, $arg1, $arg2, ...$args) {
+    if($operationType === $this::add) {
+      $total = $arg1 + $arg2;
+      foreach($args as $arg) {
+        $total += $arg;
+      }
+      return $total;
     }
-    return $total;
+    else if($operationType === $this::subtract) {
+      $total = $arg1- $arg2;
+      foreach($args as $arg) {
+        $total -= $arg;
+      }
+      return $total;
+    }
+    else if($operationType === $this::multiply) {
+      $total = $arg1 * $arg2;
+      foreach($args as $arg) {
+        $total *= $arg;
+      }
+      return $total;
+    }
+    else if($operationType === $this::divide) {
+      $total = $arg1 / $arg2;
+      foreach($args as $arg) {
+        $total /= $arg;
+      }
+      return $total;
+    }
+  }
+
+  public function add($arg1, $arg2, ...$args) {
+    return $this->doOperation($this::add, $arg1, $arg2, ...$args);
   }
 
   public function subtract($arg1, $arg2, ...$args) {
-    $total = $arg1- $arg2;
-    foreach($args as $arg) {
-      $total -= $arg;
-    }
-    return $total;
+    return $this->doOperation($this::subtract, $arg1, $arg2, ...$args);
   }
 
   public function multiply($arg1, $arg2, ...$args) {
-    $total = $arg1 * $arg2;
-    foreach($args as $arg) {
-      $total *= $arg;
-    }
-    return $total;
+    return $this->doOperation($this::multiply, $arg1, $arg2, ...$args);
   }
 
   public function divide($arg1, $arg2, ...$args) {
-    $total = $arg1 / $arg2;
-    foreach($args as $arg) {
-      $total /= $arg;
-    }
-    return $total;
+    return $this->doOperation($this::divide, $arg1, $arg2, ...$args);
   }
 }
